45536c5d1c19d7340f097f7ec280525ff6d8bc65
Pepicha <petar.syleimezqn@gmail.com>
2023-04-04 18:04:19 +0300

    Validation on Vehicle VM + New Validation Class

--- M13-Project/RentACar.ViewModels/Vehicles/CreateVehiclesVM.cs
+++ M13-Project/RentACar.ViewModels/Vehicles/CreateVehiclesVM.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using RentACar.Common;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Xml.Linq;
 
 namespace RentACar.ViewModels.Vehicles
 {
     public class CreateVehiclesVM
     {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [Display(Name = "Brand")]
         public string Brand { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [Display(Name = "Model")]
         public string Model { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{yyyy-MM}", ApplyFormatInEditMode = true)]
+        [YearAfter1886Validation]
+        [Display(Name = "Model")]
         public DateTime Year { get; set; }
+        [Required]
+        [Range(1,12)]
+        [Display(Name = "Passenger Seats")]
         public int PassengerSeats { get; set; }
+        [MaxLength(255)]
         public string Description { get; set; }
+        [Required]
+        [Range(30,100000)]
         public decimal PricePerDay { get; set; }
     }
 }
