aa510449db1ca7ae40a91463d48df7cc9d2d1ba1
Nitrum7 <nasko24@mail.bg>
2023-04-05 19:26:36 +0300

    Basic Accept/Decline func.

---
+++ M13-Project/RentACar.ViewModels/Requests/CreateRequestVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.ViewModels.Requests
+{
+    public class CreateRequestVM
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string User { get; set; }
+        public SelectList VehiclesList { get; set; }
+    }
+}
